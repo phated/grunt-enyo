9acd60b7d870236ca324c7bcc1c20ea434a4f703
Blaine Bublitz <phate666x@gmail.com>
2012-09-01 20:06:07 -0700

    a little cleanup in path construction

--- tasks/build.js
+++ tasks/build.js
@@ -3,6 +3,8 @@ module.exports = function(grunt) {
   var path = require('path');
 
   grunt.registerTask('build', 'Build your Enyo project - The Awesome Way', function(){
+    var minifyModule = path.normalize(process.cwd() + '/enyo/tools/minifier/minify.js');
+
     process.argv.pop();
     // Build process needs to know the absolute path to enyo
     process.argv.push('-enyo');
@@ -18,10 +20,10 @@ module.exports = function(grunt) {
     process.chdir('enyo/minify');
 
     // Require in the minifier to process argv and build
-    require(process.cwd() + '/../tools/minifier/minify.js');
+    require(minifyModule);
 
     // Hack to get minifier to run again later, need to do a module.exports on the minifer to fix this
-    delete require.cache[path.normalize(process.cwd() + '/../tools/minifier/minify.js')];
+    delete require.cache[minifyModule];
 
     // Remove the the arguments not related to the enyo absolute path
     process.argv.splice(4);
@@ -35,7 +37,7 @@ module.exports = function(grunt) {
     process.chdir('../../tools');
 
     // Require in the minifier (not cached because we deleted it)
-    require(process.cwd() + '/../enyo/tools/minifier/minify');
+    require(minifyModule);
   });
 
 };
